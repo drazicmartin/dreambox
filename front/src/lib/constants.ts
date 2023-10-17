461db11cc1e6f621a2a4ec399eddf06e4c9d3fab
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2023-10-17 17:33:29 +0200

    fix: env variable for production

--- front/src/lib/constants.ts
+++ front/src/lib/constants.ts
@@ -1,4 +1,3 @@
-import { PUBLIC_BACKEND_IP } from "$env/static/public"
-import { PUBLIC_BACKEND_PORT } from "$env/static/public"
+import { PUBLIC_BACKEND_IP, PUBLIC_BACKEND_PORT } from "$env/static/public";
 
-export const BACKEND_URL: string = `http://${PUBLIC_BACKEND_IP}:${PUBLIC_BACKEND_PORT}/api/`
+export const BACKEND_URL: string = `http://${process.env['PUBLIC_BACKEND_IP'] ?? PUBLIC_BACKEND_IP}:${process.env['PUBLIC_BACKEND_PORT'] ?? PUBLIC_BACKEND_PORT}/api/`;
